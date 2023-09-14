bd8d3899d7b2d1c890f9f275813142c96cadd535
Cooper Smith <coopersmi@gmail.com>
2023-09-14 16:04:05 -0400

    Last.fm
    Hat tip chatgpt for writing the JS

---
+++ main.js
@@ -0,0 +1,21 @@
+const apiKey = "c45fbeb0d318aac9d7698d798b639811"; // Replace with your Last.fm API key
+const username = "coopersmith"; // Replace with your Last.fm username
+
+
+  // API Endpoint to get top artists
+const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${username}&api_key=${apiKey}&format=json&period=3month&limit=5`;
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const artistData = data.topartists.artist;
+    const artistNames = artistData.map(artist => artist.name);
+
+    // Create a comma-separated string of artist names
+    const commaSeparatedArtists = artistNames.join(", ");
+
+    // Locate the list item and populate it
+    const listItem = document.getElementById("listening-to-item");
+    listItem.textContent = `Recently listening to a lot of ${commaSeparatedArtists}`;
+  })
+  .catch(error => console.error("Error fetching data: ", error));
