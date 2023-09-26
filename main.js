5f68aca5de880892243724a8ef900391f26230f9
Cooper Smith <coopersmi@gmail.com>
2023-09-26 10:59:37 -0400

    New

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+// Last.fm variables
 const apiKey = "c45fbeb0d318aac9d7698d798b639811"; // Replace with your Last.fm API key
 const username = "coopersmith"; // Replace with your Last.fm username
 
+// API Endpoint to get top artists from Last.fm
+const lastFmApiUrl = `http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${username}&api_key=${apiKey}&format=json&period=3month&limit=5`;
 
-  // API Endpoint to get top artists
-const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${username}&api_key=${apiKey}&format=json&period=3month&limit=5`;
-
-fetch(apiUrl)
+// Fetch most recent artists from Last.fm
+fetch(lastFmApiUrl)
   .then(response => response.json())
   .then(data => {
     const artistData = data.topartists.artist;
@@ -18,4 +19,29 @@ fetch(apiUrl)
     const listItem = document.getElementById("listening-to-item");
     listItem.textContent = `Recently listening to a lot of ${commaSeparatedArtists}`;
   })
-  .catch(error => console.error("Error fetching data: ", error));
+  .catch(error => console.error("Error fetching data from Last.fm: ", error));
+
+// Readwise variables
+const readwiseToken = '0GncVQqKSLkt1txFBbqupFvnTR4SgaiEfXVbebq5xbSw1qzK60'; // Replace with your Readwise API Token
+
+// Fetch most recent books from Readwise
+fetch('https://api.readwise.io/v2/books/', {
+  method: 'GET',
+  headers: {
+    'Authorization': `Token ${readwiseToken}`,
+  }
+})
+.then(response => {
+  if (response.status !== 200) {
+    console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+    return;
+  }
+  return response.json();
+})
+.then(data => {
+  console.log('Data from Readwise:', data);
+  // ...your code to update the page
+})
+.catch(err => {
+  console.log('Fetch Error:', err);
+});
